b9c7e5ed210b2181b5563ce5207c3c1952c87e7e
Ratchadaphorn Boonrungsee <136043081+imtheauwak@users.noreply.github.com>
2024-10-18 04:30:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1285,4 +1285,5 @@ def send_to_DB(data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use port 5000 if not specified
+    app.run(debug=True, host='0.0.0.0', port=port)
